9f637d1c1df694a375fdc1f4e7785378a828762d
BaskarM-333 <baskarmuniyappan1234@gmail.com>
2026-04-03 10:55:40 +0530

    Update sr_flipflop.v

--- sr_flipflop.v
+++ sr_flipflop.v
@@ -5,3 +5,23 @@ module sr_flipflop(input s,r,clk,output q,q_bar);
   assign q=~(w1&q_bar);
   assign q=~(w2&q);
 endmodule
+
+module sr_flipflop(input s,r,clk,output q,q_bar);
+  always @(posedge clk)begin
+    if(s&&~r)begin
+      q<=1;
+      q_bar<=0;
+    end
+    else if(~s&&r)begin
+      q<=0;
+      q_bar<=1;
+    end
+    else if(~s&&~r)begin
+      q<=q;
+      q_bar<=q_bar;
+    end
+    else begin
+      q<=1'bx;
+    end
+  end
+endmodule
